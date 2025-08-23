3e41112cb76a0b362f89e7d34b1263a6058ce3af
M Preethi <M Preethi@FFE-M-Preethi>
2025-08-23 11:58:11 +0530

    commit

---
+++ src/test/java/practice/MyPracticeTest.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class MyPracticeTest
+{
+	@Test
+	public void sampleTest()
+	{
+		System.out.println("Sample Test Example");
+	}
+
+}
